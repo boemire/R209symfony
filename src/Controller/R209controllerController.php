bccce4c3c7c35222f6c219ade65e7a6b80c9dae0
boemire <martinfauritte@gmail.com>
2025-05-22 10:36:17 +0200

    cour

--- src/Controller/R209controllerController.php
+++ src/Controller/R209controllerController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 final class R209controllerController extends AbstractController
 {
+    #[IsGranted('ROLE_USER')]
     #[Route('/r209controller', name: 'app_r209controller')]
     public function index(): Response
     {
