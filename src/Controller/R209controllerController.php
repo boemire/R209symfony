aedd210cf52579aad641bd630025f4f85afa5084
boemire <martinfauritte@gmail.com>
2025-05-23 11:43:26 +0200

    amelioration

--- src/Controller/R209controllerController.php
+++ src/Controller/R209controllerController.php
@@ -2,10 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Note;
+use App\Entity\Etat;
+use Doctrine\Common\Collections\Criteria;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 final class R209controllerController extends AbstractController
 {
@@ -13,8 +18,44 @@ final class R209controllerController extends AbstractController
     #[Route('/r209controller', name: 'app_r209controller')]
     public function index(): Response
     {
-        return $this->render('r209controller/index.html.twig', [
-            'controller_name' => 'R209controllerController',
-        ]);
+        return $this->render('r209controller/index.html.twig');
+    }
+
+    #[IsGranted('ROLE_USER')]
+    #[Route('/notes', name: 'app_notes')]
+    public function notes(EntityManagerInterface $em, UserInterface $user): Response
+    {
+        $notesEnCours = $em->createQueryBuilder()
+            ->select('n')
+            ->from(Note::class, 'n')
+            ->join('n.etat', 'e')
+            ->where('e.nom = :etat')
+            ->setParameter('etat', 'En cours')
+            ->getQuery()
+            ->getResult();
+
+        $notesUrgentes = $em->createQueryBuilder()
+            ->select('n')
+            ->from(Note::class, 'n')
+            ->join('n.tag', 't')
+            ->where('t.nom = :tag')
+            ->setParameter('tag', 'Urgent')
+            ->getQuery()
+            ->getResult();
+
+        $notesUtilisateur = $user->getNotes();
+
+        $etatEnCours = $em->getRepository(Etat::class)->findOneBy(['nom' => 'En cours']);
+        $criteria = Criteria::create()->where(Criteria::expr()->eq('etat', $etatEnCours));
+        $notesEnCoursUtilisateur = $notesUtilisateur->matching($criteria);
+
+        return $this->render('r209controller/notes.html.twig', [
+    'notes_en_cours' => $notesEnCours,
+    'notes_urgentes' => $notesUrgentes,
+    'notes_utilisateur' => $notesUtilisateur,
+    'notes_en_cours_utilisateur' => $notesEnCoursUtilisateur,
+]);
+
     }
 }
+
